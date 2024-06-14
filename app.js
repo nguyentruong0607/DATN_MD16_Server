67b9b2d8805412a0038b943c31f7ea0162258a7d
nguyentruong0607 <nguyenxuantruong2k3tb@gmail.com>
2024-06-14 23:48:18 +0700

    #6-nguyentruong0607 codeApiAndViewsAccount

--- app.js
+++ app.js
@@ -2,25 +2,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // API
 var apiHangSXRouter=require('./routes/api/hangSX.api');
 var apiSanPhamRouter=require('./routes/api/sanPham.api');
 var apiSanPhamYTRouter = require('./routes/api/sanPhamYT.api');
-var app = express();
+var apiAccountRouter = require('./routes/api/account.api');
 
+//web
+var loginRouter=require('./routes/login');
+
+var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//sesison đặt trước router 
+app.use(session({
+    secret:'nhNNGHSNFGH83sdf23435Fdzgsfnksjdfh9', // chuỗi ký tự đặc biệt để Session mã hóa, tự viết
+    resave:true,
+    saveUninitialized:true
+    }));
+    
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 // API
 app.use('/hangsx',apiHangSXRouter);
 app.use('/sanPham',apiSanPhamRouter);
 app.use('/sanPhamYT',apiSanPhamYTRouter);
+app.use('/account',apiAccountRouter);
+
+
+//web
+app.use('/',loginRouter);
+
+
+
+
 module.exports = app;
