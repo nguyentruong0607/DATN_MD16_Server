2b9f05e14ec8a7fed4135f2fa67e271ba06372fc
nguyentruong0607 <nguyenxuantruong2k3tb@gmail.com>
2024-07-07 05:12:43 +0700

    code edit sản phẩm

--- routes/sanPham.js
+++ routes/sanPham.js
@@ -6,12 +6,15 @@ var multer=require('multer');
 var spUpload=multer({dest: './tmp'});
 
 router.get('/',checkLogin.request_login,sanPhamCtrl.getAllSP);
-router.get('/chi-tiet/:id',checkLogin.request_login,sanPhamCtrl.chiTiet);
+router.get('/chi-tiet/:id',sanPhamCtrl.chiTiet);
 router.get('/add',sanPhamCtrl.add);
 
 router.post('/add',spUpload.single('hinhAnh'),sanPhamCtrl.add);
 router.get('/search',sanPhamCtrl.search);
-// router.get('/edit/:id',sanPhamCtrl.edit);
+router.get('/edit/:id',sanPhamCtrl.editSP);
 router.post('/delete/:id',sanPhamCtrl.deleteProduct);
+
+//màu
+router.put('/edit-mau/:productId/:mauId',spUpload.none(),sanPhamCtrl.updateColor);
 // Xuất router
 module.exports = router;
