4719b736d1594476131f03289578242c4a4f5119
phuongttt <tranthithuphuong0908@gmail.com>
2024-08-07 01:48:55 +0700

    update filter

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var middle=require('../middleware/check_login');
-var homeCtrl=require('../controller/home.controller');
+var middle = require("../middleware/check_login");
+var homeCtrl = require("../controller/home.controller");
 /* GET home page. */
-router.get('/', middle.request_login,homeCtrl.home);
+router.get("/", middle.request_login, homeCtrl.home);
 
 module.exports = router;
